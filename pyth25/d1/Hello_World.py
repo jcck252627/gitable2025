1a2b8232798cab09608a9eb80b4f4550ff039779
JasonC <jcck2005@gmail.com>
2025-08-26 00:52:40 +0000

    add day1

---
+++ pyth25/d1/Hello_World.py
@@ -0,0 +1,23 @@
+print("Hello World!")
+
+
+
+name = input("please enter your name: ")
+print ("Thank you " + name)
+
+
+name = "Angela"
+print ("......................and now the name becomes " +name)
+print("The length of characters of this latest name: ", len(name))
+
+print(len(input("Now SERIOUSLY what is your name? " )))
+
+print(".............Another way .....................")
+username = input ("what is your name this time? ")
+length = str(len(username))
+print("your name this time now has " + length + " characters")
+
+
+
+
+
