391657369736e47915758168062a0abd95126410
JasonC <jcck2005@gmail.com>
2025-08-26 01:04:26 +0000

    add code-ex3.py

---
+++ pyth25/d1/code-ex3.py
@@ -0,0 +1,13 @@
+glass1 = "milk"
+glass2 = "juice"
+
+print("glass1 originally has " + glass1)
+print("glass2 originally has " + glass2)
+
+placeholder=glass1
+glass1=glass2
+glass2=placeholder
+
+
+print("glass1 NOW has " + glass1)
+print("glass2 NOW has " + glass2)
