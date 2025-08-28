33d0932eb39c8990f25b23ea84c1f5d3f60b5c28
JasonC <jcck2005@gmail.com>
2025-08-28 20:54:17 +0000

    almost finish d5

---
+++ pyth25/d4/try-v32-data-structure-LIST.py
@@ -0,0 +1,17 @@
+fruits =["grape", "apple"]
+
+# Lists
+fruits = ["Cherry", "Pineapple","Banana", "Apple", "Orange"]
+vegetables = ["Spinach", "Carrot", "Cabbage", "Lettuce", "Celery"]
+
+print(fruits[0])
+print(fruits[-1])
+# Check doc.python.org for the documentation
+
+# add one item
+fruits.append("Strawberry")
+
+# add multiple items
+fruits.extend(["Watermelon", "Blueberry", "Guava"])
+print(fruits)
+
