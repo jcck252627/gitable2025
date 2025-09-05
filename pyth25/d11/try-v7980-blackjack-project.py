5451abf3e6b1c10d9c3e2e66952751a37bca0f9f
JasonC <jcck2005@gmail.com>
2025-09-05 00:22:13 +0000

    d11 started

---
+++ pyth25/d11/try-v7980-blackjack-project.py
@@ -0,0 +1,78 @@
+import random
+import art
+#card_face =["1","2","3","4","5","6","7,"8","9","10","A","K","Q","J"]
+# card_value =[1,2,3,4,5,6,7,8,9,10,11,10,10,10,10]
+
+card_face_value ={
+  "1" : 1,
+    "2": 2,
+    "3": 3,
+    "4": 4,
+    "5": 5,
+    "6": 6,
+    "7": 7,
+    "8": 8,
+    "9": 9,
+    "10": 10,
+    "A": [1,11],
+    "K": 10,
+    "Q": 10,
+    "J": 10
+}
+
+print("i am an Ace (A) for "+str(card_face_value["A"][1])+"  points if i can add it up to less than 21 " )
+
+
+
+card_face = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "A", "K", "Q", "J"]
+card_value = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, [1, 11], 10, 10, 10]
+
+print("Testing access to the diectionary__________________________________________")
+card_dict = dict(zip(card_face, card_value))
+print(card_dict["A"])  # Output: [1, 11]
+print(card_dict["A"][0])
+print(card_dict["A"][1])
+print(card_dict)
+print("___________________________________________________________________________")
+
+
+print("Welcome to the BlackJack GAME !!!!!")
+print(art.logo)
+
+
+#inital_hand_cpu = random.
+#inital_hand_player
+
+
+# Pick two random cards
+random_cards = random.sample(list(card_face_value.keys()), 2)
+
+# Calculate total value with Ace logic
+total = 0
+ace_count = 0
+
+for card in random_cards:
+    if card == "A":
+        total += 11  # Assume Ace is 11 initially
+        ace_count += 1
+    else:
+        total += card_face_value[card]
+
+# Adjust Aces if total > 21
+while total > 21 and ace_count > 0:
+    total -= 10  # Convert one Ace from 11 to 1
+    ace_count -= 1
+
+print("Random cards:", random_cards)
+print("Total value:", total)
+
+
+
+
+"""
+print(art.BLANKCARD+"  "+art.CARDS[7])
+print(art.CARDS[3])
+"""
+
+
+
