988f2d7eb6afd9301b7fb5c0ed2ab49b7591af8a
Jason Chu <jcchu@mednet.ucla.edu>
2026-02-05 16:38:45 -0800

    pyth26 start

---
+++ pyth26/cc-L2-lab-clean.py
@@ -0,0 +1,16 @@
+ 
+def print_first_name():
+    return "Elon"
+
+def print_last_name():
+    return "Musk"
+
+def print_name():
+  print(f"{print_first_name()} {print_last_name()}")  
+
+def main():
+  print_name()
+    
+
+main()
+
