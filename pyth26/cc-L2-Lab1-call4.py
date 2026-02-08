19638a71b9d15084154118fa225b694d7c92c813
Jason Chu <jcck2000@yahoo.com>
2026-02-08 09:09:17 -0800

    python lab1

---
+++ pyth26/cc-L2-Lab1-call4.py
@@ -0,0 +1,15 @@
+def four_times_four():
+   print(4*4)
+
+def four_plus_four():
+   print(4+4)
+    
+
+def do_math():
+  four_plus_four()
+  four_times_four()
+
+def main():
+   do_math()
+
+main()
---
+++ pyth26/cc-L2-lab-name-simple.py
@@ -0,0 +1,17 @@
+ 
+def print_first_name():
+    print("Elon")
+
+def print_last_name():
+    print("Musk")
+
+def print_name():
+  print_first_name()
+  print_last_name()
+
+def main():
+  print_name()
+    
+
+main()
+
