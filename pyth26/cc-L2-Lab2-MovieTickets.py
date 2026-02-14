fb6b3a57e0a28428e22b2845684a3f85fa7a60be
Jason Chu <jcck2000@yahoo.com>
2026-02-13 22:56:03 -0800

    l2lab2 movtickets done

--- pyth26/cc-L2-Lab2-MovieTickets.py
+++ pyth26/cc-L2-Lab2-MovieTickets.py
@@ -1,8 +1,8 @@
 
 # Variables (hardcoded):
-movie_name = "The Imation Game"
-adult_tickets_sold = 215
-child_tickets_sold = 76
+movie_name = "Toy Story 4"
+adult_tickets_sold = 1215
+child_tickets_sold = 666
 # Constants:
 adult_ticket_price = 10
 child_ticket_price= 6
@@ -12,8 +12,20 @@ Studio_share= 0.8
 
 adult_revenue = adult_tickets_sold * adult_ticket_price
 child_revenue = child_tickets_sold * child_ticket_price
+adult_revenue = adult_ticket_price * adult_tickets_sold
+child_revenue=child_ticket_price* child_tickets_sold
 gross_total = adult_revenue + child_revenue
 theater_profit = gross_total * 0.20
 studio_profit = gross_total * 0.80
+gross_profit    = theater_profit + studio_profit
 
-print("Movie Name:  ")
+print(f'Movie Name:         {movie_name}')
+print(f'Adult Tickets:      {adult_tickets_sold}')          
+print(f'Child Tickets:      {child_tickets_sold}')
+print(f'Adult Revenue:      ${adult_revenue:,.2f}')
+print(f'Child Revenue:      ${child_revenue:,.2f}')
+print(f'Total Revenue:      ${gross_total:,.2f}')
+print(f'Theater Profit:     ${theater_profit:,.2f}')
+print(f'Studio Profit:      ${studio_profit:,.2f}')
+print(f'Gross Profit:       ${gross_profit:,.2f}')
+
